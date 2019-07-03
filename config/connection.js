e63ea8d68ebe35a4036db0028b1369cf962328fc
Mike D <michaelp.dwyer@gmail.com>
2019-07-02 22:51:01 -0700

    New files for JAWSDB connection, updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+require("dotenv").config();
+
+var env = process.env.JAWSDB_URL;
+
 var connection;
 
-connection = mysql.createConnection({
-    host: "k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
-    port: 3306,
-    user: "v1ipivoz0rgav8ld",
-    password: "yo472142xsy1srea",
-    database: "c6zmu6o62dtm9w7q"
-
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     connection = mysql.createConnection({
-//         host: "k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
-//         port: 3306,
-//         user: "v1ipivoz0rgav8ld",
-//         password: "yo472142xsy1srea",
-//         database: "c6zmu6o62dtm9w7q"
+if (env) {
+    connection = mysql.createConnection(env);
+  } else {
+    connection = mysql.createConnection({
+      host: "k2pdcy98kpcsweia.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+      port: 3306,
+      user: "v1ipivoz0rgav8ld",
+      password: "vi4vf5dbyx4f33ei",
+      database: "c6zmu6o62dtm9w7q"
     });
-// };
+  }
 
 connection.connect(function (err) {
     if (err) {
--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
--- DROP DATABASE IF EXISTS burgers_db;
+DROP DATABASE IF EXISTS c6zmu6o62dtm9w7q;
 
--- create database burgers_db;
+create database c6zmu6o62dtm9w7q;
 
-use burgers_db;
+use c6zmu6o62dtm9w7q;
 
 create table burgers (
 	id int NOT NULL AUTO_INCREMENT,
